904d153fc762a0d6bd91813322de38b67e6980bd
bagusth15 <bagustri15@gmail.com>
2022-08-10 16:35:42 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/register', // mau diganti jadi apa ?
+        destination: '/auth/register', // path lama ?
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
